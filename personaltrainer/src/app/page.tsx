c0919f66cfca2ef6c21018998d339711742ce5f1
Arnaud De Brock <44091018+Arnazio@users.noreply.github.com>
2023-10-23 01:12:57 +0200

    Landing page navbar

---
+++ personaltrainer/src/app/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from 'react';
+import NavigationBar from './landingpage/components/NavigationBar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default HomePage;
